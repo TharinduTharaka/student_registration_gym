bfe7c697b8fedf31506e5aef8f794076e4787053
tharakakiu2023 <tharaka@kiu.ac.lk>
2023-04-27 20:10:09 +0530

    UI update

--- php/action_page.php
+++ php/action_page.php
@@ -1,4 +1,26 @@
 <?php
 
-echo $_GET['subject'];
+
+//echo $txtName;
+
+$con = mysqli_connect('localhost', 'root', '','db_student_gym');
+
+
+// get the post records
+// $txtName = $_POST['txtName'];
+// $txtEmail = $_POST['txtEmail'];
+// $txtPhone = $_POST['txtPhone'];
+// $txtMessage = $_POST['txtMessage'];
+$txtName = $_POST['fname'];
+// database insert SQL code
+$sql = "INSERT INTO `tbl` ( `fname`) VALUES ('$txtName')";
+
+// insert in database
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+
 ?>
